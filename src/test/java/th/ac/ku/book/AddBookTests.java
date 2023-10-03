fe040b13a0efb05b5f7fd4b9e9665c7f0642aac4
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-10-03 19:03:08 +0700

    Refractor AddBookTests.java and add name, student ID

--- src/test/java/th/ac/ku/book/AddBookTests.java
+++ src/test/java/th/ac/ku/book/AddBookTests.java
@@ -1,3 +1,4 @@
+// Matupol Choochotiros 6310450638
 package th.ac.ku.book;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -6,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -23,6 +26,18 @@ public class AddBookTests {
     private static WebDriver driver;
     private static WebDriverWait wait;
 
+    @FindBy(id = "nameInput")
+    private WebElement nameField;
+
+    @FindBy(id = "authorInput")
+    private WebElement authorField;
+
+    @FindBy(id = "priceInput")
+    private WebElement priceField;
+
+    @FindBy(id = "submitButton")
+    private WebElement submitButton;
+
     @BeforeAll
     public static void beforeAll() {
         WebDriverManager.chromedriver().setup();
@@ -33,28 +48,11 @@ public class AddBookTests {
     @BeforeEach
     public void beforeEach() {
         driver.get("http://localhost:" + port + "/book/add");
-    }
-
-    @AfterEach
-    public void afterEach() throws InterruptedException {
-        Thread.sleep(3000);
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        if (driver != null)
-            driver.quit();
+        PageFactory.initElements(driver, this);
     }
 
     @Test
     void testAddBook() {
-        WebElement nameField = wait.until(webDriver ->
-                webDriver.findElement(By.id("nameInput")));
-        WebElement authorField = driver.findElement(By.id("authorInput"));
-        WebElement priceField = driver.findElement(By.id("priceInput"));
-
-        WebElement submitButton = driver.findElement(By.id("submitButton"));
-
         nameField.sendKeys("Clean Code");
         authorField.sendKeys("Robert Martin");
         priceField.sendKeys("600");
@@ -71,5 +69,16 @@ public class AddBookTests {
         assertEquals("Clean Code", name.getText());
         assertEquals("Robert Martin", author.getText());
         assertEquals("600.00", price.getText());
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        if (driver != null)
+            driver.quit();
     }
 }
